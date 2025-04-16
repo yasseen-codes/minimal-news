4c0b07729ead0989a08162e6c1c38676f74fe989
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-04-16 15:22:47 +0300

    added logo component

---
+++ components/logo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <h1
+      className="text-6xl font-bold bg-gradient-to-r 
+from-[#A0845C] via-[#A0845C] to-[#F1C29B] 
+dark:from-[#d2918b] dark:to-[#5f5155] dark:via-[#d2918b] 
+bg-clip-text text-transparent"
+    >
+      Pastel HN
+    </h1>
+  );
+};
+
+export default Logo;
