731c9663e462d2a0b8d9d57af90986c8130d1af2
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-04-16 15:49:15 +0300

    added nav component

---
+++ components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+const Nav = () => {
+  return (
+    <nav>
+      <ThemeToggle />
+    </nav>
+  );
+};
+
+export default Nav;
