74286235bdeba949d363375e87e4adf5e3e75b85
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-04-16 15:48:37 +0300

    changed logo font

--- components/logo.tsx
+++ components/logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
       className="text-6xl font-bold bg-gradient-to-r 
 from-[#A0845C] via-[#A0845C] to-[#F1C29B] 
 dark:from-[#d2918b] dark:to-[#5f5155] dark:via-[#d2918b] 
-bg-clip-text text-transparent"
+bg-clip-text text-transparent font-serif"
     >
       Pastel HN
     </h1>
