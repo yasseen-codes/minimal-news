e2e3f0fa20d1759965fbdcac4078b1dd4e7c7772
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-04-22 12:58:30 +0300

    added api and hn get types

---
+++ types/api.ts
@@ -0,0 +1,9 @@
+import { HNStory } from "@/types/hn";
+
+export type ApiResponse = {
+  page: number;
+  totalPages: number;
+  stories: HNStory[];
+};
+
+export type routeValue = "top" | "new" | "ask" | "show";
---
+++ types/hn.ts
@@ -0,0 +1,9 @@
+export type HNStory = {
+  id: number;
+  title: string;
+  url?: string;
+  score: number;
+  descendants: number;
+  time: number;
+  by: string;
+};
