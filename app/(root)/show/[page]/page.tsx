ccd2eccac13c69221d305f9679edb151ebdf2dee
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-04-28 18:53:08 +0300

    style: wrap Stories component in Suspense for improved loading experience

--- app/(root)/show/[page]/page.tsx
+++ app/(root)/show/[page]/page.tsx
@@ -1,4 +1,6 @@
+import { StoriesListSkeleton } from "@/components/skeletons";
 import Stories from "@/components/stories";
+import { Suspense } from "react";
 
 export default async function Page({
   params,
@@ -6,5 +8,9 @@ export default async function Page({
   params: Promise<{ page: string }>;
 }) {
   const page = parseInt((await params).page || "1");
-  return <Stories page={page} route="show" />;
+  return (
+    <Suspense fallback={<StoriesListSkeleton />}>
+      <Stories page={page} route="show" />
+    </Suspense>
+  );
 }
