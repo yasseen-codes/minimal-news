be9381ca19a238014d8bbc42bf12ea6e899e1e97
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-05-01 01:07:57 +0300

    style: refactor Logo component to use function declaration and improve layout

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
-const Logo = () => {
+function Logo() {
   return (
-    <h1 className="font-mono text-xl font-bold whitespace-nowrap md:text-4xl">
+    <h1 className="flex gap-1 font-mono text-xl font-bold whitespace-nowrap md:text-4xl">
       Minimal News
+      <span className="md:animate-caret-blink sr-only md:not-sr-only">_</span>
     </h1>
   );
-};
+}
 
 export default Logo;
