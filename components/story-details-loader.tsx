062ba16e77220d0e1c731eb9a2ddadf4f24ebde7
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-05-01 01:22:44 +0300

    style: clean up StoryDetailsLoader component by removing unnecessary comments and enhancing structure

--- components/story-details-loader.tsx
+++ components/story-details-loader.tsx
@@ -1,6 +1,5 @@
 // components/StoryContentLoader.tsx
 
-// This is a Server Component, it does not need 'use client'
 import { notFound } from "next/navigation";
 import StoryDetails from "@/components/story-details";
 import { fetchStory } from "@/lib/data";
@@ -12,15 +11,17 @@ export default async function StoryDetailsLoader({
   storyId: string;
 }) {
   // Fetch the story data using the API route
-  // This fetch call will cause *this* component to suspend
   const story = await fetchStory(storyId);
 
-  // If the story was not found, render the Next.js notFound page
   if (!story) {
     notFound();
   }
 
   // Render the StoryDetails component, passing the fetched story data
   // This part only runs after the fetch is complete
-  return <StoryDetails story={story} />;
+  return (
+    <div className="animate-in fade-in-35 duration-300">
+      <StoryDetails story={story} />
+    </div>
+  );
 }
