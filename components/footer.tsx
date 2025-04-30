3b47faf5fa37b5e22c814a708edebe0dd17579c2
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-05-01 00:44:56 +0300

    style: remove unnecessary comments and improve code clarity in Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,38 +1,39 @@
 // components/Footer.tsx
 
-import Link from "next/link"; // Import Link for navigation
+import { CodeXml, Github, Linkedin } from "lucide-react";
+import Link from "next/link";
 
 export function Footer() {
   return (
-    // Footer container with flex layout to justify content between
     <footer className="text-muted-foreground mx-4 my-6 flex items-center justify-between p-2 text-sm md:mx-25 md:my-10 md:text-base">
       <div className="flex items-center gap-4">
         <Link
-          href="https://linkedin.com/in/yasseen-aborasheed-11592a236" // <-- Replace with your LinkedIn profile URL
+          href="https://linkedin.com/in/yasseen-aborasheed-11592a236"
           target="_blank" // Open in a new tab
           rel="noopener noreferrer" // Security best practice for external links
-          className="hover:text-accent font-mono" // Apply mono font and hover underline
+          className="hover:text-accent flex items-center gap-1 font-mono"
         >
+          <Linkedin size={16} className="sr-only md:not-sr-only" />
           LinkedIn
         </Link>
-
         <Link
-          href="https://github.com/yasseen-codes" // <-- Replace with your GitHub profile URL
-          target="_blank" // Open in a new tab
-          rel="noopener noreferrer" // Security best practice for external links
-          className="hover:text-accent font-mono" // Apply mono font and hover underline
+          href="https://github.com/yasseen-codes"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-accent flex items-center gap-1 font-mono"
         >
+          <Github size={16} className="sr-only md:not-sr-only" />
           GitHub
         </Link>
       </div>
-
       <Link
         href="/"
-        target="_blank" // Open in a new tab
-        rel="noopener noreferrer" // Security best practice for external links
-        className="hover:text-accent font-mono"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-accent flex items-center gap-1 font-mono"
       >
         Source Code
+        <CodeXml size={20} className="sr-only md:not-sr-only" />
       </Link>
     </footer>
   );
