1b17afb522090e2b90f5aeeea5704bb37827fe15
yasseen-codes <yasseenaborasheed8@gmail.com>
2025-05-16 06:09:53 +0300

    feat: add Toaster component for user notifications

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <Sonner
+      theme={resolvedTheme as ToasterProps["theme"]}
+      className="toaster group"
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
